b3ce1cd62961a66d74c603633e1bb04a22902ce3
BruciX <brct93@gmail.com>
2023-02-11 16:48:50 -0500

    Volver a suar el conversor de temperatura

--- src/MonedayTiempo.java
+++ src/MonedayTiempo.java
@@ -3,7 +3,6 @@ import javax.swing.JOptionPane;
 
 public class MonedayTiempo {
     public static void main(String[] args) {
-        DecimalFormat FormatearDecimal = new DecimalFormat("#.##");
 
         Object[] options = { "Convertir Monedas", "Conversor de Temperatura", "Salir" };
         int choice = JOptionPane.showOptionDialog(null, "Que Quieres hacer?", "Convertir",
@@ -14,6 +13,7 @@ public class MonedayTiempo {
             System.exit(0);
         }
         if (choice == 0) {
+            DecimalFormat FormatearDecimal = new DecimalFormat("#.##");
             boolean continuarPrograma = true;
             while (continuarPrograma) {
                 String[] opciones = { "Soles a Euros", "Soles a Dólares", "Soles a Libras Esterlinas", "Soles a Yen",
@@ -59,37 +59,43 @@ public class MonedayTiempo {
 
         }
         if (choice == 1) {
-            String[] opciones = { "Celsius a Fahrenheit", "Fahrenheit a Celsius" };
-            int opcion = JOptionPane.showOptionDialog(null, "Elige una opción de conversión:",
-                    "Convertidor de Temperatura", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
-                    null, opciones, opciones[0]);
 
-            String valorStr = JOptionPane.showInputDialog("Ingresa el valor a convertir: ");
+            Boolean SeguirPrograma = true;
+            while (SeguirPrograma) {
 
-            double valor = 0;
-            try {
-                valor = Double.parseDouble(valorStr);
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Valor no válido");
-                System.exit(0);
-            }
+                String[] opciones = { "Celsius a Fahrenheit", "Fahrenheit a Celsius" };
+                int opcion = JOptionPane.showOptionDialog(null, "Elige una opción de conversión:",
+                        "Convertidor de Temperatura", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                        null, opciones, opciones[0]);
 
-            double resultado = 0;
-            if (opcion == 0) {
-                resultado = (valor * 9 / 5) + 32;
-                JOptionPane.showMessageDialog(null,
-                        valor + " grados Celsius son " + resultado + " grados Fahrenheit");
-            } else if (opcion == 1) {
-                resultado = (valor - 32) * 5 / 9;
-                JOptionPane.showMessageDialog(null,
-                        valor + " grados Fahrenheit son " + resultado + " grados Celsius");
-            }
+                String valorStr = JOptionPane.showInputDialog("Ingresa el valor a convertir: ");
 
-            int continuar = JOptionPane.showConfirmDialog(null, "¿Deseas continuar usando el programa?", "",
-                    JOptionPane.YES_NO_CANCEL_OPTION);
-            if (continuar == JOptionPane.NO_OPTION || continuar == JOptionPane.CANCEL_OPTION) {
-                JOptionPane.showMessageDialog(null, "Programa Finalizado");
-                System.exit(0);
+                double valor = 0;
+                try {
+                    valor = Double.parseDouble(valorStr);
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Valor no válido");
+                    System.exit(0);
+                }
+
+                double resultado = 0;
+                if (opcion == 0) {
+                    resultado = (valor * 9 / 5) + 32;
+                    JOptionPane.showMessageDialog(null,
+                            valor + " grados Celsius son " + resultado + " grados Fahrenheit");
+                } else if (opcion == 1) {
+                    resultado = (valor - 32) * 5 / 9;
+                    JOptionPane.showMessageDialog(null,
+                            valor + " grados Fahrenheit son " + resultado + " grados Celsius");
+                }
+
+                int continuar = JOptionPane.showConfirmDialog(null, "¿Deseas continuar usando el programa?", "",
+                        JOptionPane.YES_NO_CANCEL_OPTION);
+                if (continuar == JOptionPane.NO_OPTION || continuar == JOptionPane.CANCEL_OPTION) {
+                    SeguirPrograma = false;
+                    JOptionPane.showMessageDialog(null, "Programa Finalizado");
+
+                }
             }
 
         }
